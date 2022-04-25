bdab99f2ddcc0a0d945ca38a22b0fce42ec9f98e
shamil-am <shamil.mammadov1@gmail.com>
2022-04-25 15:27:34 +0400

    navbar added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,27 @@
-  <!-- <h1>Home {{ $store.state.user.name }}</h1> -->
 <template>
   <div class="home">
-    <v-card class="ml-auto" height="400" width="256">
-      <h1 style="text-align: center">{{ $store.state.user && $store.state.user.name }}</h1>
-      <v-navigation-drawer class="deep-purple accent-4" dark permanent>
+    <v-app-bar color="deep-purple accent-4" dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-menu left bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon v-bind="attrs" v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
         <v-list>
           <v-list-item v-for="item in items" :key="item.title" link>
             <v-list-item-icon>
@@ -13,15 +31,17 @@
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item-content>
           </v-list-item>
+          <v-list-item link @click="logOut">
+            <v-list-item-icon>
+              <v-icon>{{ "mdi-logout-variant" }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ "Logout" }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
         </v-list>
-
-        <template v-slot:append>
-          <div class="pa-2">
-            <v-btn block @click="logOut"> Logout </v-btn>
-          </div>
-        </template>
-      </v-navigation-drawer>
-    </v-card>
+      </v-menu>
+    </v-app-bar>
   </div>
 </template>
 
